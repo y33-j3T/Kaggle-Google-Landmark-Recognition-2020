545db259484763f843a8cc10218e469a60119b51
y33-j3T <yeejet.tech@gmail.com>
2020-10-21 22:46:46 +0800

    make data preprocessing pipeline

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,20 +1,13 @@
-# run as script
-# preprocess data and put them to their respective folders
-
 import os
-import re
 import shutil
-import sys
-import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-
-from keras import layers, models, optimizers
-from keras.preprocessing.image import ImageDataGenerator
-from keras.preprocessing import image
-from keras.applications import ResNet50V2, VGG16
+import sys
 from glob import glob
 
+
+#########
+# Paths #
+#########
 # base directory
 BASE_DIR = '..'
 
@@ -30,112 +23,156 @@ PROCESSED_DIR = os.path.join(DATA_DIR, 'processed')
 TRAIN = os.path.join(RAW_DIR, 'train')
 TEST = os.path.join(RAW_DIR, 'test')
 DF_TRAIN = os.path.join(TRAIN, 'train.csv')
+LANDMARK_INDICES100 = os.path.join(RAW_DIR, 'landmark_indices100.csv')
+SAMPLE_SUBMISSION = os.path.join(RAW_DIR, 'sample_submission.csv')
 
 # interim data
 TRAIN_BY_CLASS = os.path.join(INTERIM_DIR, 'train')
 VALIDATION_BY_CLASS = os.path.join(INTERIM_DIR, 'validation')
 TEST_BY_CLASS = os.path.join(INTERIM_DIR, 'test')
 
+# trained models on cloud
+XCEPTION_100 = 'xception_100epoch.h5'
+RESNET50V2_100 = 'model2_100epoch.h5'
+RESNET50V2_10 = 'model2_10epoch.h5'
 
-def make_dir_by_class():
+#######################
+# Data Pre-processing #
+#######################
+def _clean_filenames(filenames):
+    
+    res = []
+    
+    for name in filenames:
+        try:
+            basename = os.path.basename(name)
+            new_name = os.path.splitext(basename)[0]
+            res.append((name, new_name))
+            
+        except AttributeError:
+            print('Not found')
+    
+    return res
+
+
+def mkdir_by_class(remove_old=False):
+    """
+    Prepare train, validation & test directories to contain all image classes.
+
+    Parameters:
+        remove_old: If true, remove old directory of classes with all images in it.
+    """
+
     try:
+        if remove_old and os.path.exists(TRAIN_BY_CLASS):
+            shutil.rmtree(TRAIN_BY_CLASS)
+        if remove_old and os.path.exists(VALIDATION_BY_CLASS):
+            shutil.rmtree(VALIDATION_BY_CLASS)
+        if remove_old and os.path.exists(TEST_BY_CLASS):
+            shutil.rmtree(TEST_BY_CLASS)
+
         os.mkdir(TRAIN_BY_CLASS)
         os.mkdir(VALIDATION_BY_CLASS)
         os.mkdir(TEST_BY_CLASS)
+        
     except OSError as e:
         print(e)
 
 
-def get_df_train():
-    def clean_f_names(f_names):
-
-        res = []
-        for name in f_names:
-            try:
-                basename = os.path.basename(name)
-                new_name = os.path.splitext(basename)[0]
-                res.append((name, new_name))
-            except AttributeError:
-                print('Not found')
-        return res
-
+def make_df_train():
+    """
+    Make train DataFrame with image path, filename & landmark id.
+    """
     df_map = pd.read_csv(DF_TRAIN)
-
     im_train = glob(os.path.join(TRAIN, '*/*/*/*.jpg'))
-    im_test = glob(os.path.join(TEST, '*/*/*/*.jpg'))
-
-    im_train2 = clean_f_names(im_train)
-    im_test2 = clean_f_names(im_test)
-
-    df_train_fname = pd.DataFrame(im_train2, columns=['path', 'f_name'])
-    df_test_fname = pd.DataFrame(im_test2, columns=['path', 'f_name'])
-
-    df_train = df_train_fname.merge(df_map, left_on='f_name', right_on='id')
+    im_train = _clean_filenames(im_train)
+    df_train_fname = pd.DataFrame(im_train, columns =['path', 'filename'])
+    df_train = df_train_fname.merge(df_map, left_on='filename', right_on='id')
+    return df_train
 
 
-# only use landmark_id with > N samples
-def get_df_train_sample_larger_than(df_train, n=100):
-    vc = df_train['landmark_id'].value_counts() > n
+def split_dataset(df, num_min_img, num_validation, num_test):
+    """
+    Split images into train, validation and test dataset.
+
+    Parameters:
+        df: DataFrame with containing image paths, filenames & corresponding landmark of images. 
+            Column names should be `path`, `filename` & `landmark_id`.
+        num_min_img: The least amount of train images in any class.
+        num_validation: The number of validation images in any class.
+        num_test: The number of test images in any class
+    """
+
+    # select classes >= the specified minimum number of images
+    df['landmark_id'] = df['landmark_id'].astype(str)
+    vc = df['landmark_id'].value_counts() >= num_min_img
     vc = vc[vc]
-    res = df_train.loc[df_train['landmark_id'].isin(vc.index)]
-    return res
-
-
-# seperate training images into their respective landmark_id folders
-def copy_img_to_training_class_dir(df_train_sampled):
-    for i, row in df_train_sampled.iterrows():
-
+    df_filtered = df.loc[df['landmark_id'].isin(vc.index)]
+    
+    # seperate train img into class directories
+    for _, row in df_filtered.iterrows():
         class_dir = os.path.join(TRAIN_BY_CLASS, row['landmark_id'])
 
         if not os.path.exists(class_dir):
             os.mkdir(class_dir)
 
-        shutil.copy(row['path'], class_dir)
-
-
-# select n samples for validation (note: n must be smaller than training data sampled)
-def cut_img_to_validation_class_dir(n=50):
+        src = row['path']
+        dst = class_dir
+        
+        shutil.copy(src, dst)
+    
+    # list of images classes
     classes_id = os.listdir(TRAIN_BY_CLASS)
 
+    # cut out images from train to validation & test
+    num_to_move = num_validation + num_test
     for class_id in classes_id:
         train_class_dir = os.path.join(TRAIN_BY_CLASS, class_id)
         validation_class_dir = os.path.join(VALIDATION_BY_CLASS, class_id)
+        test_class_dir = os.path.join(TEST_BY_CLASS, class_id)
 
         if not os.path.exists(validation_class_dir):
-            os.mkdir(validation_class_dir)
+            os.mkdir(validation_class_dir)  
 
-        files_to_move = glob(os.path.join(train_class_dir, '*'))[:n]
+        if not os.path.exists(test_class_dir):
+            os.mkdir(test_class_dir)
 
-        for f in files_to_move:
+        files = glob(os.path.join(train_class_dir, '*'))[:num_to_move]
+        files_validation = files[:num_validation]
+        files_test = files[num_validation:]
+
+        for f in files_validation:
             shutil.move(f, validation_class_dir)
+
+        for f in files_test:
+            shutil.move(f, test_class_dir)
 
 
 # show total classes & images for training & validation after seperation
-def overview_data():
-    im_train_classes_dir = glob(os.path.join(TRAIN_BY_CLASS, '*'))
-    im_validation_classes_dir = glob(os.path.join(VALIDATION_BY_CLASS, '*'))
+def overview_dataset():
+    train = glob(os.path.join(TRAIN_BY_CLASS, '*'))
+    validation = glob(os.path.join(VALIDATION_BY_CLASS, '*'))
+    test = glob(os.path.join(TEST_BY_CLASS, '*'))
 
-    total_training_images = sum(
-        len(os.listdir(d)) for d in im_train_classes_dir)
-    total_validation_images = sum(
-        len(os.listdir(d)) for d in im_validation_classes_dir)
+    total_train = sum(len(os.listdir(d)) for d in train)
+    total_validation = sum(len(os.listdir(d)) for d in validation)
+    total_test = sum(len(os.listdir(d)) for d in validation)
 
-    print('total training classes: ', len(im_train_classes_dir))
-    print('total validation classes: ', len(im_train_classes_dir))
-    print('total training images: ', total_training_images)
-    print('total validation images: ', total_validation_images)
-
-
-# main
-def generate_data(min_training_sample, num_validation_sample):
-    make_dir_by_class()
-    df_train = get_df_train()
-    df_train_sampled = get_df_train_sample_larger_than(df_train,
-                                                       min_training_sample)
-    copy_img_to_training_class_dir(df_train_sampled)
-    cut_img_to_validation_class_dir(num_validation_sample)
+    print('total train classes: ', len(train))
+    print('total validation classes: ', len(validation))
+    print('total test classes: ', len(test))
+    print('total train images: ', total_train)
+    print('total validation images: ', total_validation)
+    print('total test images: ', total_test)
 
 
 if __name__ == "__main__":
-    # usage: `python preprocess.py <min_training_sample> <num_validation_sample>`
-    generate_data(sys.argv[1], sys.argv[2])
+    # USE: python preprocess.py <min images> <num validation> <num test>
+    min_img = int(sys.argv[1])
+    num_validation = int(sys.argv[2])
+    num_test = int(sys.argv[3])
+    
+    mkdir_by_class(remove_old=True)
+    df_train = make_df_train()
+    split_dataset(df_train, min_img, num_validation, num_test)
+    overview_dataset()
